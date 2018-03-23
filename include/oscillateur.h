031fa1d6e5d49ac2c9981042c260a89cda28016f
jreboud <jeehanne.reboud@gmail.com>
2018-03-23 16:26:04 +0100

    cinque

--- include/oscillateur.h
+++ include/oscillateur.h
@@ -5,12 +5,16 @@
 #include "Vecteur.h"
 
 class Oscillateur{
+	// contructeur ??
+	
 	public:
-	Vecteur fonction(int t);
+	Vecteur fonction(double to);
+	Vecteur getposition const() ;
+	Vecteur getvitesse const ();
 	private:
 	
 	Vecteur P;// le degres de liberte est donne par la taille de ce vecteur 
-	Vecteur Q;// 
+	Vecteur Q;// tableau de la premiere derivee du vecteur P
 };
 
 
--- sources/oscillateur.cc
+++ sources/oscillateur.cc
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "oscillateur.h"
-Vecteur Oscillateur::fonction(int t){
+Vecteur Oscillateur::fonction(double to){
 	
 	return t*(P+Q);
 }
-	
+Vecteur Oscillateur:: getposition const() {return P;}
+Vecteur Oscillateur:: getvitesse const ();{retrun Q;}
 	
