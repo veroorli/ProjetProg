998bb034a1bdd10a9868d6b9ef8bfb4a93c8f601
jreboud <jeehanne.reboud@gmail.com>
2018-04-19 18:45:34 +0200

    hhh

--- fichierCC/systeme.cc
+++ fichierCC/systeme.cc
@@ -1,11 +1,17 @@
 #include <iostream>
 #include "dessinable.h"
 #include "supportdessin.h"
+#include "systeme.h"
+#include <vector>
+#include "oscillateur.h"
 
 using namespace std;
 
-Systeme( vector<Oscillateur> liste_oscillateur,Integrateur* integrateur = nullptr, Supportdessin* support = nullptr)://probeleme
+Systeme::Systeme ( vector<Oscillateur> liste_oscillateur,Integrateur* integrateur = nullptr, Supportdessin* support = nullptr)://probeleme
  liste_oscillateur(liste_oscillateur), integrateur(integrateur), support(support) {}
- ; 
+ ;
+ 
+ virtual void Systeme::dessine() override
+  { support->dessine(*this); } 
  
 	
