c76c3553d59ff1fd67c8a253d499143d0ff6137e
jreboud <jeehanne.reboud@gmail.com>
2018-04-19 17:17:24 +0200

    supportdessin

---
+++ fichierCC/supportdessin.h
@@ -0,0 +1,14 @@
+
+//incomplete 
+class Supportdessin {
+	
+	public:
+	//constructeur :: attributs??
+	//DESTRUCTEUR
+	virtual dessine(Pendule pendule const&)=0;
+	virtual dessine(Ressort ressort const&)=0;
+	//virtual dessine(Torsion torsion const&)=0
+	
+	
+	
+}
